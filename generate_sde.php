ba44d26119cd51b1e52358b99d96ae30c37bcd13
SimplyUnnamed <lgullestrup@gmail.com>
2021-09-04 20:18:21 +1000

    initial Commit

---
+++ generate_sde.php
@@ -0,0 +1,21 @@
+<?php
+
+$sde_name = 'sde-20210713-TRANQUILITY';
+
+$data = [
+    'version'   => $sde_name,
+    'url'       => 'https://www.fuzzworks.co.uk/dump/:version/',
+    'format'    => '.sql.bz2',
+    'tables'    => [
+        "invCategories",
+        "invNames",
+        "invTypes",
+        "mapDenormalize",
+        "mapSolarSystemJumps",
+        "staStations",
+    ],
+];
+
+$file = __DIR__ ."/sde.json";
+$sde = json_encode($data).PHP_EOL;
+file_put_contents($file, $sde, LOCK_EX);
